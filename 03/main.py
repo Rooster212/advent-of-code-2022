12946df11e5bc314d54c9b088ee6b124d591c612
Jamie Roos <jamieroos1@gmail.com>
2022-12-06 23:27:03 +0000

    Working part 1 of day 3

--- 03/main.py
+++ 03/main.py
@@ -9,5 +9,28 @@ def retrieve_items_from_line(line):
   line_as_array = [char for char in line]
   half_length = len(line_as_array) // 2
   first_half, second_half = line_as_array[:half_length], line_as_array[half_length:]
-  return (first_half, second_half)
+  return list(set(first_half).intersection(second_half))
 
+def priorities_for_array(section_list):
+  sum = 0
+  for item in section_list:
+    sum += letter_to_priority(item)
+  return sum
+
+def process(input_file_contents):
+  priority_sum = 0
+  for line in input_file_contents:
+    if not line.strip():
+      break
+    else:
+      items = retrieve_items_from_line(line)
+      priority_sum += priorities_for_array(items)
+  return priority_sum
+
+def main():
+  with open("input.txt") as f:
+    result = process(f)
+    print(result)
+
+if __name__ == '__main__':
+  main()
--- 03/test.py
+++ 03/test.py
@@ -17,9 +17,51 @@ class SimpleTest(unittest.TestCase):
     result = main.letter_to_priority('c')
     self.assertEqual(result, 3)
 
-  def test_calculateline(self):
+  # examples from day page
+  def test_retrieve_items_from_line_1(self):
     result = main.retrieve_items_from_line("vJrwpWtwJgWrhcsFMMfFFhFp")
-    print(result)
+    self.assertEqual(result, ['p'])
+  def test_retrieve_items_from_line_2(self):
+    result = main.retrieve_items_from_line("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+    self.assertEqual(result, ['L'])
+  def test_retrieve_items_from_line_3(self):
+    result = main.retrieve_items_from_line("PmmdzqPrVvPwwTWBwg")
+    self.assertEqual(result, ['P'])
+  def test_retrieve_items_from_line_4(self):
+    result = main.retrieve_items_from_line("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")
+    self.assertEqual(result, ['v'])
+  def test_retrieve_items_from_line_5(self):
+    result = main.retrieve_items_from_line("ttgJtRGJQctTZtZT")
+    self.assertEqual(result, ['t'])
+  def test_retrieve_items_from_line_6(self):
+    result = main.retrieve_items_from_line("CrZsJsPPZsGzwwsLwLmpwMDw")
+    self.assertEqual(result, ['s'])
+
+  def test_priorities_for_array_1(self):
+    result = main.priorities_for_array(['a'])
+    self.assertEqual(result, 1)
+  def test_priorities_for_array_2(self):
+    result = main.priorities_for_array(['a', 'A'])
+    self.assertEqual(result, 28)
+  def test_priorities_for_array_1(self):
+    result = main.priorities_for_array(['a'])
+    self.assertEqual(result, 1)
+  # example from day page
+  def test_priorities_for_array_3(self):
+    result = main.priorities_for_array(['p', 'L', 'P', 'v', 't', 's'])
+    self.assertEqual(result, 157)
+
+  def test_process_array(self):
+    test_data = [
+      "vJrwpWtwJgWrhcsFMMfFFhFp",
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+      "PmmdzqPrVvPwwTWBwg",
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw"
+    ]
+    result = main.process(test_data)
+    self.assertEqual(result, 157)
 
 if __name__ == '__main__':
   unittest.main()
