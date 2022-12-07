d076438e9632564f091a22ca2feaa7ee9309dda2
Jamie Roos <jamieroos1@gmail.com>
2022-12-07 22:00:20 +0000

    Working part 2 of day 4

--- 04/main.py
+++ 04/main.py
@@ -4,7 +4,7 @@ def get_upper_lower_as_numbers(input):
   split_numbers = str(input).split('-')
   return (int(split_numbers[0]), int(split_numbers[1]))
 
-def line_does_overlap(input_line):
+def line_does_fully_overlap(input_line):
   input_line = str(input_line).split(',')
   first_lower, first_upper = get_upper_lower_as_numbers(input_line[0])
   second_lower, second_upper = get_upper_lower_as_numbers(input_line[1])
@@ -18,19 +18,44 @@ def line_does_overlap(input_line):
 
   return False
 
+def line_does_overlap(input_line):
+  input_line = str(input_line).split(',')
+  first_lower, first_upper = get_upper_lower_as_numbers(input_line[0])
+  second_lower, second_upper = get_upper_lower_as_numbers(input_line[1])
+
+  if(first_lower <= second_lower and second_lower <= first_upper):
+    return True
+  elif(first_lower <= second_upper and second_lower <= first_upper):
+    return True
+  elif(second_lower <= first_lower and first_lower <= second_upper):
+    return True
+  elif(second_upper <= first_upper and first_upper <= second_upper):
+    return True
+
+  return False
+
+
 def main():
   with open("input.txt") as f:
     fully_contained_count = 0
+    partially_contained_count = 0
 
     for line in f:
       if not line.strip():
         break
       else:
-        result = line_does_overlap(line)
+        # part 1
+        result = line_does_fully_overlap(line)
         if(result == True):
           fully_contained_count += 1
 
-    print(fully_contained_count)
+        # part 2
+        result = line_does_overlap(line)
+        if(result == True):
+          partially_contained_count+= 1
+
+    print("Fully contained count: " + str(fully_contained_count))
+    print("Partially contained count: " + str(partially_contained_count))
 
 if __name__ == '__main__':
   main()
--- 04/test.py
+++ 04/test.py
@@ -12,6 +12,25 @@ class SimpleTest(unittest.TestCase):
     self.assertEqual(result, (8, 51))
 
   # Examples from day 4 page
+  def test_line_does_fully_overlap_1(self):
+    result = main.line_does_fully_overlap('2-4,6-8')
+    self.assertEqual(result, False)
+  def test_line_does_fully_overlap_2(self):
+    result = main.line_does_fully_overlap('2-3,4-5')
+    self.assertEqual(result, False)
+  def test_line_does_fully_overlap_3(self):
+    result = main.line_does_fully_overlap('5-7,7-9')
+    self.assertEqual(result, False)
+  def test_line_does_fully_overlap_4(self):
+    result = main.line_does_fully_overlap('2-8,3-7')
+    self.assertEqual(result, True)
+  def test_line_does_fully_overlap_5(self):
+    result = main.line_does_fully_overlap('6-6,4-6')
+    self.assertEqual(result, True)
+  def test_line_does_fully_overlap_6(self):
+    result = main.line_does_fully_overlap('2-6,4-8')
+    self.assertEqual(result, False)
+
   def test_line_does_overlap_1(self):
     result = main.line_does_overlap('2-4,6-8')
     self.assertEqual(result, False)
@@ -20,7 +39,7 @@ class SimpleTest(unittest.TestCase):
     self.assertEqual(result, False)
   def test_line_does_overlap_3(self):
     result = main.line_does_overlap('5-7,7-9')
-    self.assertEqual(result, False)
+    self.assertEqual(result, True)
   def test_line_does_overlap_4(self):
     result = main.line_does_overlap('2-8,3-7')
     self.assertEqual(result, True)
@@ -29,7 +48,7 @@ class SimpleTest(unittest.TestCase):
     self.assertEqual(result, True)
   def test_line_does_overlap_6(self):
     result = main.line_does_overlap('2-6,4-8')
-    self.assertEqual(result, False)
+    self.assertEqual(result, True)
 
 if __name__ == '__main__':
   unittest.main()
