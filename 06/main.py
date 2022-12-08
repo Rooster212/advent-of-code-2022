c26fdb2cee8ac655a111143e49f0970fd89176f4
Jamie Roos <jamieroos1@gmail.com>
2022-12-08 23:02:54 +0000

    Working part 2 of day 6 with optimisations/cleanup

--- 06/main.py
+++ 06/main.py
@@ -1,14 +1,26 @@
 def get_marker_part_1(input: str):
-    input_length = len(input)
-
-    i = 3
-    while i < input_length:
-        i += 1
-        characters = input[i-4:i]
-        if (len(set(characters)) == 4):
-            return i
+    return get_location_in_string(input, 4)
 
 
-with open("input.txt") as f:
-    input = f.read().removesuffix('\n')
-    print(get_marker_part_1(input))
+def get_marker_part_2(input: str):
+    return get_location_in_string(input, 14)
+
+
+def get_location_in_string(input: str, desired_marker_length: int):
+    for i in range(len(input)):
+        characters = input[i:i+desired_marker_length]
+        if len(set(characters)) == desired_marker_length:
+            return i+desired_marker_length
+
+
+def main():
+    with open("input.txt") as f:
+        input = f.read().removesuffix('\n')
+        print("Start of packet marker - end index | " +
+              str(get_marker_part_1(input)))
+        print("Start of message marker - end index | " +
+              str(get_marker_part_2(input)))
+
+
+if __name__ == '__main__':
+    main()
--- 06/test.py
+++ 06/test.py
@@ -23,6 +23,49 @@ class SimpleTest(unittest.TestCase):
         result = main.get_marker_part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
         self.assertEqual(result, 11)
 
+    def test_get_location_in_string_1(self):
+        result = main.get_location_in_string(
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4)
+        self.assertEqual(result, 7)
+
+    def test_get_location_in_string_2(self):
+        result = main.get_location_in_string("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)
+        self.assertEqual(result, 5)
+
+    def test_get_location_in_string_3(self):
+        result = main.get_location_in_string("nppdvjthqldpwncqszvftbrmjlhg", 4)
+        self.assertEqual(result, 6)
+
+    def test_get_location_in_string_4(self):
+        result = main.get_location_in_string(
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)
+        self.assertEqual(result, 10)
+
+    def test_get_location_in_string_5(self):
+        result = main.get_location_in_string(
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)
+        self.assertEqual(result, 11)
+
+    def test_get_marker_part_2_1(self):
+        result = main.get_marker_part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+        self.assertEqual(result, 19)
+
+    def test_get_marker_part_2_2(self):
+        result = main.get_marker_part_2("bvwbjplbgvbhsrlpgdmjqwftvncz")
+        self.assertEqual(result, 23)
+
+    def test_get_marker_part_2_3(self):
+        result = main.get_marker_part_2("nppdvjthqldpwncqszvftbrmjlhg")
+        self.assertEqual(result, 23)
+
+    def test_get_marker_part_2_4(self):
+        result = main.get_marker_part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+        self.assertEqual(result, 29)
+
+    def test_get_marker_part_2_5(self):
+        result = main.get_marker_part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+        self.assertEqual(result, 26)
+
 
 if __name__ == '__main__':
     unittest.main()
