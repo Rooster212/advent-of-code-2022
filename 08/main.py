02c95b9fe7c13f7cd495364b9e904acf746fd890
Jamie Roos <jamieroos1@gmail.com>
2022-12-08 17:57:05 +0000

    working part 2 of day 8

--- 08/main.py
+++ 08/main.py
@@ -18,15 +18,18 @@ def find_visible_trees_count(input_lines):
     height = len(grid)
     width = len(grid[0])
 
-    # Width - 2 to avoid double counting corner trees
-    num_visible_trees += (height * 2) + ((width - 2) * 2)
+    highest_scenic_score = int(0)
 
     # Count the number of visible trees in the interior of the grid
-    for row in range(1, len(grid)-1):
-        for col in range(1, len(grid[row])-1):
-            if (process_tree(grid, (row, col))):
+    for row in range(height):
+        for col in range(width):
+            tree_visible, scenic_score = process_tree(grid, (row, col))
+            if (tree_visible):
                 num_visible_trees += 1
-    return num_visible_trees
+            if (scenic_score > highest_scenic_score):
+                highest_scenic_score = scenic_score
+
+    return num_visible_trees, highest_scenic_score
 
 
 def process_tree(trees_grid, key: tuple):
@@ -37,39 +40,56 @@ def process_tree(trees_grid, key: tuple):
     row_count = len(trees_grid)
     col_count = len(trees_grid[0])
 
+    # Edge trees
+    # Always visible, and scenic score is 0
+    if (row == 0) or (col == 0) or (row == row_count-1) or (row == row_count-1):
+        return (True, 0)
+
     # look left
     left_visible = True
+    left_view_dist = 0
     for c in range(col-1, -1, -1):
+        left_view_dist += 1
         if trees_grid[row][c] >= tree_height:
             left_visible = False
             break
 
     # look right
     right_visible = True
+    right_view_dist = 0
     for c in range(col+1, col_count):
+        right_view_dist += 1
         if trees_grid[row][c] >= tree_height:
             right_visible = False
             break
 
     up_visible = True
+    up_view_dist = 0
     for r in range(row-1, -1, -1):
+        up_view_dist += 1
         if trees_grid[r][col] >= tree_height:
             up_visible = False
             break
 
     down_visible = True
+    down_view_dist = 0
     for r in range(row+1, row_count):
+        down_view_dist += 1
         if trees_grid[r][col] >= tree_height:
             down_visible = False
             break
 
     visible = (left_visible or right_visible or up_visible or down_visible)
-    return visible
+    scenic_score = left_view_dist * right_view_dist * up_view_dist * down_view_dist
+
+    return (visible, scenic_score)
 
 
 def main():
     with open("input.txt") as f:
-        print(find_visible_trees_count(f))
+        visible_tree_count, highest_scenic_score = find_visible_trees_count(f)
+        print("Visible trees: " + str(visible_tree_count))
+        print("Highest scenic score: " + str(highest_scenic_score))
 
 
 if __name__ == '__main__':
--- 08/test.py
+++ 08/test.py
@@ -1,18 +1,29 @@
 import unittest
 import main
 
+
 class SimpleTest(unittest.TestCase):
 
-  def test_find_visible_trees(self):
-    result = main.find_visible_trees_count([
-      "30373",
-      "25512",
-      "65332",
-      "33549",
-      "35390",
-    ])
-    self.assertEqual(result, 21)
+    def test_find_visible_trees_count(self):
+        result, _ = main.find_visible_trees_count([
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390",
+        ])
+        self.assertEqual(result, 21)
+
+    def test_find_visible_trees_scenic_score(self):
+        _, result = main.find_visible_trees_count([
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390",
+        ])
+        self.assertEqual(result, 8)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
