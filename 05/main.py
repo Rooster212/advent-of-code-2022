ee8b3a56881c5ab1b98d13993cccb232e06eb883
Jamie Roos <jamieroos1@gmail.com>
2022-12-08 21:21:09 +0000

    Working part 1 of day 5 (using hardcoded stacks)

--- 05/main.py
+++ 05/main.py
@@ -1,35 +1,62 @@
 def get_challenge_input_stacks():
-  stacks = [
-    ['V','R','H','B','G','D','W'],
-    ['F','R','C','G','N','J'],
-    ['J','N','D','H','F','S','L'],
-    ['V','S','D','J'],
-    ['V','N','W','Q','R','D','H','S'],
-    ['M','C','H','G','P'],
-    ['C','H','Z','L','G','B','J','F'],
-    ['R','J','S'],
-    ['M','V','N','B','R','S','G','L']
-  ]
+    stacks = [
+        ['V', 'R', 'H', 'B', 'G', 'D', 'W'],
+        ['F', 'R', 'C', 'G', 'N', 'J'],
+        ['J', 'N', 'D', 'H', 'F', 'S', 'L'],
+        ['V', 'S', 'D', 'J'],
+        ['V', 'N', 'W', 'Q', 'R', 'D', 'H', 'S'],
+        ['M', 'C', 'H', 'G', 'P'],
+        ['C', 'H', 'Z', 'L', 'G', 'B', 'J', 'F'],
+        ['R', 'J', 'S'],
+        ['M', 'V', 'N', 'B', 'R', 'S', 'G', 'L']
+    ]
 
-  # We reverse all the stacks so that append() and pop() work as stack operators
-  for stack in stacks:
-    stack.reverse()
+    # We reverse all the stacks so that append() and pop() work as stack operators
+    for stack in stacks:
+        stack.reverse()
 
-  return stacks
+    return stacks
+
+
+def step_parse(line: str):
+    # step line looks like the following:
+    # "move 1 from 2 to 1"
+    line_parts = line.removesuffix('\n').split(' ')
+    return (int(line_parts[1]), int(line_parts[3]), int(line_parts[5]))
+
+
+def process_stacks_and_steps(stacks, steps):
+    for line in steps:
+        move_count, from_stack, to_stack = step_parse(line)
+        for _i in range(move_count):
+            item_to_move = stacks[from_stack-1].pop()
+            stacks[to_stack-1].append(item_to_move)
+            print_stacks(stacks)
+    return stacks
+
+
+def print_stacks(stacks):
+    stack_count = 1
+    for stack in stacks:
+        print(str(stack_count) + " | " + str(stack))
+        stack_count += 1
+    print("----------------")
+
 
 def main():
-  print(get_challenge_input_stacks())
+    stacks = get_challenge_input_stacks()
 
-  # stacks = []
-  # stack_count = 3
+    print("input stacks")
+    print_stacks(stacks)
 
-  # # Initialise stacks
-  # for stack_number in range(stack_count):
-  #   stacks[stack_number] = []
-
-
-  # # with open("input.txt") as f:
+    with open("input_steps.txt") as f:
+        result = process_stacks_and_steps(stacks, f)
+        stack_count = 1
+        for stack in result:
+            top_item = stack[len(stack)-1]
+            print(str(stack_count) + " | " + top_item)
+            stack_count += 1
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- 05/test.py
+++ 05/test.py
@@ -4,10 +4,45 @@ import main
 
 class SimpleTest(unittest.TestCase):
 
-  def test_get_challenge_input_stacks(self):
-    result = main.get_challenge_input_stacks()
-    self.assertEqual(len(result), 9)
+    def get_test_stacks(self):
+        # I have reversed these to work for testing
+        stacks = [
+            ['Z', 'N'],
+            ['M', 'C', 'D'],
+            ['P'],
+        ]
+        return stacks
+
+    def get_test_steps(self):
+        return [
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2",
+        ]
+
+    def test_get_challenge_input_stacks(self):
+        result = main.get_challenge_input_stacks()
+        self.assertEqual(len(result), 9)
+
+    def test_step_parse_1(self):
+        result = main.step_parse('move 1 from 2 to 3')
+        self.assertEqual(result, (1, 2, 3))
+
+    def test_step_parse_2(self):
+        result = main.step_parse('move 5 from 12 to 51')
+        self.assertEqual(result, (5, 12, 51))
+
+    def test_step_parse_3(self):
+        result = main.step_parse('move 15 from 2 to 13')
+        self.assertEqual(result, (15, 2, 13))
+
+    def test_run(self):
+        stacks = self.get_test_stacks()
+        steps = self.get_test_steps()
+        result = main.process_stacks_and_steps(stacks, steps)
+        main.print_stacks(result)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
